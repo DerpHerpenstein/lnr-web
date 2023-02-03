f57819bff5349a7667e7d482c8ad284121f52f15
derp <derp@derpfinance.com>
2023-02-03 13:13:47 -0500

    Updated to version 0.1.1 -Updated ABI to use the updated lnr-web smart contract -updated the index to: --Use the updated ABI --use multipart files to support files up to 256kB gzipped (three.js) --added import map parsing to allow for ES6 module importing

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ const pako = require("pako");
  */
 class LNR_WEB {
 
+  static get LNR_ZERO_HASH(){
+	 return "0x0000000000000000000000000000000000000000000000000000000000000000";
+	}
+
   static get LNR_WEB_ADDRESS() {
-    return "0x9B1558c57Bf2B2686f2E024252E84BA746eBa665";
+    return "0xc72FDddeCf69D37d58518727B70BD616BC795Ca3";//"0x9B1558c57Bf2B2686f2E024252E84BA746eBa665";
   }
 
   constructor(_ethers, _provider, _signer) {
@@ -32,35 +36,46 @@ class LNR_WEB {
     return uncompressed;
   }
 
-  async getDataFromChain(tmpTxHash, tmpDataHash){
+  async getRawDataFromChain(tmpTxHash, tmpDataHash){
     let tx = await this.provider.getTransaction(tmpTxHash);
     let input_data = '0x' + tx.data.slice(10);
     let iface = new this.ethers.utils.Interface(lnrWebAbi);
     let decodedData = iface.parseTransaction({ data: tx.data, value: tx.value });
     let params = decodedData.args;
-    for(let i=0; i< params[0].length; i++){
-      if(params[0][i] === tmpDataHash){
-        let finalData = params[4][i];
-        if(params[5][i]) // if its zipped, unzip it
-          finalData = this.decompressData(params[4][i]);
+    return params;
+  }
 
-        //finalData = decodeURI(finalData); // makes the files larger
-        let computedHash = this.ethers.utils.keccak256(this.ethers.utils.toUtf8Bytes(finalData));
-        if(computedHash === params[0][i])
-          return {
-            hash: params[0][i],
-            name: params[1][i],
-            type: params[2][i],
-            desc: params[3][i],
-            raw : params[4][i],
-            data: finalData,
-            zip: params[5][i]
-          }
-        else{
-          throw "Error: Hash Mismatch! Possibly malicious file"
+  async getDataFromChain(tmpTxHash, tmpDataHash){
+    let params = await this.getRawDataFromChain(tmpTxHash, tmpDataHash)
+		let params1 = null;
+    if(params[0] === tmpDataHash){
+      let finalData = params[5];
+			if(params[1] !== LNR_WEB.LNR_ZERO_HASH){
+				params1 = (await this.getRawDataFromChain(params[1], tmpDataHash))
+				finalData += params1[5].slice(2);
+			}
+
+      if(params[5]) // if its zipped, unzip it
+        finalData = this.decompressData(finalData);
+			console.log(finalData);
+
+      //finalData = decodeURI(finalData); // makes the files larger
+      let computedHash = this.ethers.utils.keccak256(this.ethers.utils.toUtf8Bytes(finalData));
+      if(computedHash === params[0])
+        return {
+          hash: params[0],
+          name: params[2],
+          type: params[3],
+          desc: params[4],
+          raw : ((params[1] !== LNR_WEB.LNR_ZERO_HASH)? (params[5] +params1[5].slice(2)) : params[5]),
+          data: finalData,
+          zip: params[6]
         }
+      else{
+        throw "Error: Hash Mismatch! Possibly malicious file"
       }
     }
+
     throw "Error: Unable to locate asset: derp://" + tmpTxHash + "/" + tmpDataHash;
   }
 
@@ -75,16 +90,39 @@ class LNR_WEB {
     tmpNewFile.uncompressedKeccak256 = this.ethers.utils.keccak256(this.ethers.utils.toUtf8Bytes(tmpNewFile.uncompressedData));
     //console.log("Upload Asset:");
     //console.log(tmpNewFile);
-
-    return this.lnrWebContract.uploadAssets(  [tmpNewFile.uncompressedKeccak256],
-                                              [tmpNewFile.name],
-                                              [tmpNewFile.type],
-                                              [tmpNewFile.desc],
-                                              [tmpNewFile.compressedData],
-                                              [true] ).then(function(result){
+    if(tmpNewFile.compressedData.length < 127000){
+      return this.lnrWebContract.uploadAsset( tmpNewFile.uncompressedKeccak256,
+                                              LNR_WEB.LNR_ZERO_HASH,
+                                              tmpNewFile.name,
+                                              tmpNewFile.type,
+                                              tmpNewFile.desc,
+                                              tmpNewFile.compressedData,
+                                              true ).then(function(result){
                                                   return result;
                                               });
+    }
+    else if(tmpNewFile.compressedData.length < 254000){
+      // 128kb max geth tx size, make sure we fit into two tx's
+      const half = Math.ceil(tmpNewFile.compressedData.length / 2);
+      let secondHalf = await this.lnrWebContract.uploadAsset(tmpNewFile.uncompressedKeccak256,
+                                              LNR_WEB.LNR_ZERO_HASH,
+                                              tmpNewFile.name,
+                                              tmpNewFile.type,
+                                              tmpNewFile.desc,
+                                              tmpNewFile.compressedData.slice(half),
+                                              true );
 
+      let firstHalf = await this.lnrWebContract.uploadAsset(tmpNewFile.uncompressedKeccak256,
+                                              secondHalf.hash,
+                                              tmpNewFile.name,
+                                              tmpNewFile.type,
+                                              tmpNewFile.desc,
+                                              tmpNewFile.compressedData.slice(0,half),
+                                              true );
+      return firstHalf;
+    }
+    else
+      throw "File too large, The current max is 254kb";
     //console.log("Upload Asset Result:")
     //console.log(upload);
     //console.log('txHash', upload.hash);
@@ -146,11 +184,25 @@ class LNR_WEB {
   async replaceJS(site, viewIt){
     let regexp = '<script (.*?)src="([^">]+)">[\s\S]*?<\/script>';
     let matches = site.matchAll(regexp);
+		// find script tags and inject input into them
     for (const match of matches) {
       let tmpData = await this.fetchBase64Data(match[2]);
       if(viewIt || !tmpData[0])
         site = site.replace(match[2], "data:text/javascript;base64, " + tmpData[1]);
     }
+		// find the importmap and inject code into it
+		let start = site.indexOf('<script type="importmap">');
+		let end = site.indexOf('</script>', start)
+		if(start && end){
+			let importJSON = JSON.parse(site.slice(start+25, end));
+			for(const tmpImport in importJSON.imports){
+				let tmpURL = importJSON.imports[tmpImport];
+				let tmpData = await this.fetchBase64Data(tmpURL);
+				if(viewIt || !tmpData[0])
+					site = site.replace(tmpURL, "data:text/javascript;base64, " + tmpData[1]);
+			}
+
+		}
     return site;
   }
 }
